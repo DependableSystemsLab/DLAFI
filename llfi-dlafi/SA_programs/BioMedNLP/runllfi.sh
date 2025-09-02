c62b9848a83e9c99ed14844984de9c203bc39133
manisadati <sadati.man@outlook.com>
2025-09-02 21:17:21 +0000

    fix runllfi.sh

--- llfi-dlafi/SA_programs/BioMedNLP/runllfi.sh
+++ llfi-dlafi/SA_programs/BioMedNLP/runllfi.sh
@@ -9,9 +9,9 @@ deactivate
 echo "All files copied successfully."
 $LLFI_BUILD_ROOT/bin/instrument --readable -L $ONNX_MLIR_BUILD/Debug/lib -lcruntime -ljson-c -lonnx_proto -lprotobuf model.ll
 echo "Instrumented model.ll to model-instrumented.ll"
-$LLFI_BUILD_ROOT/bin/profile ./llfi/model-profiling.exe "input${$1}_0.pb" "input${$1}_1.pb" "input${$1}_2.pb" 0
+$LLFI_BUILD_ROOT/bin/profile ./llfi/model-profiling.exe "input${1}_0.pb" "input${1}_1.pb" "input${1}_2.pb" 0
 echo "Generated model-profiling.exe"
-$LLFI_BUILD_ROOT/bin/injectfault ./llfi/model-faultinjection.exe "input${$1}_0.pb" "input${$1}_1.pb" "input${$1}_2.pb" 0
+$LLFI_BUILD_ROOT/bin/injectfault ./llfi/model-faultinjection.exe "input${1}_0.pb" "input${1}_1.pb" "input${1}_2.pb" 0
 echo "Generated model-faultinjection.exe"
 
 rm -f input*_*.pb
