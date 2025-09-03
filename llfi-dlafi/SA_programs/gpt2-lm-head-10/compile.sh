1cf77dd6440c50fd11076f77e0a921611e8e42eb
manisadati <sadati.man@outlook.com>
2025-09-03 18:20:14 +0000

    fix compile.sh

--- llfi-dlafi/SA_programs/gpt2-lm-head-10/compile.sh
+++ llfi-dlafi/SA_programs/gpt2-lm-head-10/compile.sh
@@ -1,18 +1,17 @@
-printf "\n[Compile Script]: Getting the ONNX model\n"
-FILE=model.onnx
+
+printf "[Compile Script]: Getting the ONNX model\n"
+FILE=gpt2-lm-head-10.onnx
+
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else
     echo "$FILE does not exist."
-    source $DLAFI_ROOT/pytorch-fi/pytorch-env/bin/activate
-    python3 -m transformers.onnx --model=microsoft/codebert-base-mlm --feature=masked-lm  --opset 14 .
-    deactivate
-
+    wget https://github.com/onnx/models/raw/main/validated/text/machine_comprehension/gpt-2/model/gpt2-lm-head-10.onnx
 fi
 
 printf "\n[Compile Script]: Convert TF model to LLVM IR\n"
-onnx-mlir --EmitLLVMIR  --instrument-onnx-ops="ALL" --InstrumentBeforeAndAfterOp model.onnx --lltfi-fi
-mlir-translate -mlir-to-llvmir model.onnx.mlir > model.mlir.ll
+onnx-mlir --EmitLLVMIR  --instrument-onnx-ops="ALL" --InstrumentBeforeAndAfterOp gpt2-lm-head-10.onnx --lltfi-fi
+mlir-translate -mlir-to-llvmir gpt2-lm-head-10.onnx.mlir > model.mlir.ll
 
 printf "\n[Compile Script]: Compile main driver program and link to TF model in LLVM IR\n"
 clang++ -DONNX_ML=1 input.c -o main.ll -O0 -S -emit-llvm -lonnx_proto -lprotobuf -I$ONNX_MLIR_SRC/include
